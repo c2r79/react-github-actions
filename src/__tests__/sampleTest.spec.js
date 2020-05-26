2678e52e67126e35b96180b0a128bd31574f0fe7
c2r79 <carlos.r.romero7@gmail.com>
2020-05-26 01:53:04 -0400

    added an add function

--- src/__tests__/sampleTest.spec.js
+++ src/__tests__/sampleTest.spec.js
@@ -1,5 +1,13 @@
+import addingPies from "../common/testFunctions";
+
 describe("This is just a sample test", () => {
   it("should pass easily", () => {
     expect(45 * 10).toEqual(450);
+  });
+});
+
+describe("Testing sample function", () => {
+  it("should work as expected", () => {
+    expect(addingPies(7, 10)).toEqual(17);
   });
 });
