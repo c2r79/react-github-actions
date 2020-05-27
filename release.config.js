bef059169375750153a6327099eef4be0b316e4c
c2r79 <carlos.r.romero7@gmail.com>
2020-05-27 18:06:49 -0400

    adding semantic versioning

---
+++ release.config.js
@@ -0,0 +1,9 @@
+export default {
+  branches: ["master"],
+  repositoryUrl: "https://github.com/c2r79/react-github-actions",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/github",
+    "@semantic-release/release-notes-generator",
+  ],
+};
