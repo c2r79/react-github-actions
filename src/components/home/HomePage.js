da2ca89809d5f50b5221732a9faf6f6db34fbc72
c2r79 <carlos.r.romero7@gmail.com>
2020-05-27 19:16:46 -0400

    feat: do i need the parenthesis

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => (
   // * The jumbotron className comes w/ bootstrap which gives a large greeting
   <div className="jumbotron">
     <h1>React Github Actions</h1>
-    <h3>Added this to test ci flow</h3>
+    <h2>Added this to test ci flow</h2>
     <p>This is just a react sample app</p>
 
     <Link to="about" className="btn btn-primary btn-lg">
