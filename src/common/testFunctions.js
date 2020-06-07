ac0522d0593a65215c8a8072f98f54e19e32603d
Carlos <cromero@renttherunway.com>
2020-06-07 00:56:18 -0400

    fix(sonarQube): still trying

--- src/common/testFunctions.js
+++ src/common/testFunctions.js
@@ -1,9 +1,14 @@
 const subt = (a, b) => a - b;
+const mult = (a, b) => a * b;
+
 const userInfo = {
   username: "testz",
   password: "testz",
 };
 
+const test = mult(3, 12);
+
 console.log(userInfo);
+console.log(test);
 
 export default (a, b) => a + b;
